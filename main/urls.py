5884fcde4cf890ae0db70ee7300f704163a65a00
Sabirov Asadbek <sabirovasadbek932848802@gmail.com>
2023-11-29 12:09:29 +0500

    first commit

---
+++ main/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path("", index, name='home'),
+    path("about/", about, name='about'),
+    path("blog/", blog, name='blog'),
+    path("blog/d/", blog_details, name='blog-details'),
+    path("checkout/", checkout, name='checkout'),
+    path("contact/", contact, name='contact'),
+    path("faq/", faq, name='faq'),
+    path("history/", history, name='history'),
+    path("menu/", menu, name='menu'),
+    path("reservation/", reservation, name='reservation'),
+    path("shop-details/", shop_details, name='shop-details'),
+    path("shop-sidebar/", shop_sidebar, name='shop-sidebar'),
+    path("shop/", shop, name='shop'),
+    path("team/", team, name='team'),
+]
---
+++ main/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def blog(request):
+    return render(request, "blog.html")
+
+
+def blog_details(request):
+    return render(request, 'blog-details.html')
+
+
+def checkout(request):
+    return render(request, "checkout.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
+
+
+def faq(request):
+    return render(request, "faq.html")
+
+
+def history(request):
+    return render(request, "history.html")
+
+
+def menu(request):
+    return render(request, "menu.html")
+
+
+def reservation(request):
+    return render(request, "reservation.html")
+
+
+def shop_details(request):
+    return render(request, "shop-details.html")
+
+
+def shop_sidebar(request):
+    return render(request, "shop-sidebar.html")
+
+
+def shop(request):
+    return render(request, "shop.html")
+
+
+def team(request):
+    return render(request, "team.html")
